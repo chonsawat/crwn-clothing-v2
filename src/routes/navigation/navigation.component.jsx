15378071f292e7bd99b99548b3183e5ffb630059
chonsawat <chonsawat.nakanam@kkumail.com>
2023-04-17 04:24:43 +0700

    add: navigation and rotues to another page

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div>
+          <Link className="nav-links-container" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
